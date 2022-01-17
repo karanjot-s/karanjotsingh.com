f93391df2955ff56cc87f54c9c7cbdd50da387cf
Karanjot Singh <66475280+karanjot-s@users.noreply.github.com>
2022-01-17 16:48:18 +0530

    added api

--- next.config.js
+++ next.config.js
@@ -1,18 +1,11 @@
-const path = require("path");
-const withPWA = require("next-pwa");
-
-module.exports = withPWA({
+module.exports = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, "node_modules")],
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/api/:path*",
+      },
+    ];
   },
-  images: {
-    domains: ["images.unsplash.com"],
-  },
-  pwa: {
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-  },
-});
+};
